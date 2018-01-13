497aa1cc20391756c4c2b5e3bc375eb9654c690b
Fernando Schaeffer-Araújo <fernando_araujo@me.com>
2018-01-13 00:31:35 -0800

    Initial Commit

---
+++ fantastip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fantastip
+//
+//  Created by Fernando Schaeffer-Araujo  on 1/13/18.
+//  Copyright © 2018 Fernando Schaeffer-Araujo . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
